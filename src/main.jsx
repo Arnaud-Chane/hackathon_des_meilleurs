5227d3d97612a73206f55fd6676ea71972680f46
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-05-09 17:06:09 +0200

    First commit clean project

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/scss/main.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Homepage from './pages/Homepage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
