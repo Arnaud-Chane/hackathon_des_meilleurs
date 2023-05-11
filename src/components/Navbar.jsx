a0e7e3493d1fe635d13d0f543f125fc8d9ec4515
marine <marinou.boulanger@gmail.com>
2023-05-11 11:52:55 +0200

    nav bar without logo
    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-
-function Navbar() {
-  return <div>Navbar</div>;
+export default function NavBar() {
+  return (
+    <div className="nav-container">
+      <div className="logo-website">
+        <div>
+          logo <br /> img
+        </div>
+        <h1>Mystic trips</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>Far far away</li>
+          <li>Middle earth</li>
+          <li>Caribbean cruise</li>
+          <li>Wizard’s</li>
+          <li>Pandora</li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
-
-export default Navbar;
