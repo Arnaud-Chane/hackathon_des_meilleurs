5f16857f8dc3fa0845d990d77593c3ea5322d1e6
Gabriel Le_Rest <gabriellerest@gmail.com>
2023-05-11 12:25:37 +0200

    préparation de la page qui va recevoir les mondes

---
+++ src/components/TextDecrivePlanet.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TextDecrivePlanet() {
+  return (
+    <div className="textDecrivePlanet">
+      <h1 className="textDecrivePlanetTitle">Titre</h1>
+      <p className="textDecrivePlanetParagraphe">
+        BlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBla
+      </p>
+    </div>
+  );
+}
+export default TextDecrivePlanet;
