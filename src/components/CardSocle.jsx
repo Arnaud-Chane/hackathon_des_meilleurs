5314dd30b2455aa8111ced335748abc691f3e508
Gabriel Le_Rest <gabriellerest@gmail.com>
2023-05-11 11:18:51 +0200

    Homepage préparer

---
+++ src/components/CardSocle.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function CardSocle({ transform }) {
+  return <div className="CardSocle" style={{ transform }}></div>;
+}
+export default CardSocle;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from "react";
 import getAPIData from "../API/FetchData";
+import CardSocle from "../components/CardSocle";
 
 function Homepage() {
+  const transformCard = [
+    "rotate3d(2, -8, -1, 50deg) scale(1) matrix(2, 0, 0, 1, 45, 35)",
+    "rotate3d(2, -8, -1, 30deg) scale(1.1) matrix(1.5, 0, 0, 1, 10, 0)",
+    "rotate3d(0, 0, 0, 0deg) scale(1.2) scaleX(1.5)",
+    "rotate3d(2, 8, 1, 30deg) scale(1.1)  matrix(1.5, 0, 0, 1, -10, 0)",
+    "rotate3d(1, 10, 1, 50deg) scale(1) matrix(2, 0, 0, 1, -45, 35)"
+  ];
+
   const [dataAPI, setDataAPI] = useState([]);
 
   useEffect(() => {
@@ -14,7 +23,9 @@ function Homepage() {
 
   return (
     <div className="Homepage">
-      Coucou tout le monde ! Avec des bisous bien sûr !
+      {transformCard.map((itemCard, index) => (
+        <CardSocle transform={itemCard} key={index} />
+      ))}
     </div>
   );
 }
