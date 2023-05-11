f49e678bd6a7ef465a93c7da835e525a37003040
marine <marinou.boulanger@gmail.com>
2023-05-11 15:59:12 +0200

    card

--- src/pages/GetPageFivePlanet.jsx
+++ src/pages/GetPageFivePlanet.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import TextDecrivePlanet from "../components/TextDecrivePlanet";
+import Card from "../components/Card";
+import nimbus from "../assets/images/Wizard/nimbus.svg";
+import cheminette from "../assets/images/Wizard/cheminette.svg";
 
 function GetPageFivePlanet() {
+  const [dataWorld, setDataWorld] = useState([]);
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("dataWorld"));
+    if (data) {
+      setDataWorld(data);
+    }
+  }, []);
+
   return (
     <div className="GetPageFivePlanet">
       <TextDecrivePlanet />
+      <Card cardFront={nimbus} cardBack={cheminette} />
     </div>
   );
 }
