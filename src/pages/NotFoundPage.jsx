a6fccd3a6625eddbafd1cc230c280ec7da0804f8
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-05-12 10:20:28 +0200

    Button home

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,24 +1,14 @@
-// import { Redirect } from "react-router-dom";
-// import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import error from "../assets/images/Group3.svg";
 
 export default function NotFoundPage() {
-  // const [redirected, setRedirected] = useState(false);
-
-  // useEffect(() => {
-  //   const redirectTimer = setTimeout(() => {
-  //     setRedirected(true);
-  //   }, 5000); // redirect after 5 seconds
-  //   return () => clearTimeout(redirectTimer); // cleanup timer on unmount
-  // }, []);
-
-  // if (redirected) {
-  //   return <Redirect to="/" />;
-  // }
 
   return (
     <div className="NotFoundPage">
       <img src={error}  alt="404" />
+      <Link to="/">
+        <div className="home-redirect">Home</div>
+      </Link>
     </div>
   );
 }
